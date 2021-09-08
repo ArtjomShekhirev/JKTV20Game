5817fb552ef39299b294aec4fe4b40980a5a5f9d
alekseiKromski <aleksei.kromski@ivkhk.ee>
2021-09-08 11:37:49 +0300

    Use of if

--- src/game/Game.java
+++ src/game/Game.java
@@ -5,6 +5,9 @@
  */
 package game;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
@@ -15,7 +18,17 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Random random = new Random();
+        int number = random.nextInt(9-0+1)+0;
+        System.out.println("Program stored number in range of 0 to 9, test you'r luck!");
+        Scanner scanner = new Scanner(System.in);
+        int userNumber = scanner.nextInt();
+        if (number == userNumber) {
+            System.out.println("You win!");
+        }else{
+            System.out.println(":, Wrong, right number was:" +number);
+                    }
+        
     }
     
 }
