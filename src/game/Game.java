6c6119bd197d1ecf45b0aa88226339d702671439
alekseiKromski <aleksei.kromski@ivkhk.ee>
2021-09-13 10:19:01 +0300

    3 attemps

--- src/game/Game.java
+++ src/game/Game.java
@@ -18,17 +18,27 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+          int attempt = 0;
         Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
         int number = random.nextInt(9-0+1)+0;
         System.out.println("Program stored number in range of 0 to 9, test you'r luck!");
-        Scanner scanner = new Scanner(System.in);
-        int userNumber = scanner.nextInt();
-        if (number == userNumber) {
-            System.out.println("You win!");
-        }else{
-            System.out.println(":, Wrong, right number was:" +number);
-                    }
+        for (int i = 0; i < 3; i++){
         
+            int userNumber = scanner.nextInt();
+            if (number == userNumber) {
+                System.out.println("You win!");
+                break;
+            }else{
+                if(attempt < 2){
+                    System.out.println("Wrong, try again!");
+                }else{
+                    System.out.println("You lose!");
+                }
+
+            }attempt++;  
+
+        }
     }
-    
 }
+    
